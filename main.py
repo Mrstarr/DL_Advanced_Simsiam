b850f783dc4367756ba851966db082b891cb9bce
Emma Lind <emmal8@kth.se>
2021-11-10 08:57:15 +0100

    started with train script

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import torch
+import torch.nn as nn
+
+# custom import
 import models.simsiam_builder
 
 batch_size = 16
 lr = 0.1
+gpu = None
 
 
 '''
@@ -11,6 +16,6 @@ print("Creating SimSiam model...")
 simsiam =  models.simsiam_builder.SimSiam()
 
 # infer learning rate before changing batch size
-init_lr = args.lr * args.batch_size / 256
+init_lr = lr * batch_size / 256
 # define loss function (criterion) and optimizer
-criterion = nn.CosineSimilarity(dim=1).cuda(args.gpu)
+criterion = nn.CosineSimilarity(dim=1).cuda(gpu)
--- train.py
+++ train.py
@@ -1,20 +1,15 @@
 
 
-
-
-
-def train_simsiam(train_loader, model, criterion, optimizer, epoch):
+def train_simsiam(train_loader, model, criterion, optimizer, epoch, gpu):
     for i, (images, _) in enumerate(train_loader):
 
-        if args.gpu is not None:
-            images[0] = images[0].cuda(args.gpu, non_blocking=True)
-            images[1] = images[1].cuda(args.gpu, non_blocking=True)
+        if gpu is not None:
+            images[0] = images[0].cuda(gpu, non_blocking=True)
+            images[1] = images[1].cuda(gpu, non_blocking=True)
 
         # compute output and loss
         p1, p2, z1, z2 = model(x1=images[0], x2=images[1])
         loss = -(criterion(p1, z2).mean() + criterion(p2, z1).mean()) * 0.5
-
-        losses.update(loss.item(), images[0].size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
