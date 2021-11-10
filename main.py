800f51e0b2ed387c0fe1442da72a9fe04e2c0d49
Emma Lind <emmal8@kth.se>
2021-11-10 10:32:52 +0100

    started with train script

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import torch.nn as nn
 
 # custom import
 import models.simsiam_builder
+from train import train_simsiam, adjust_learning_rate
+from dataloader import load_cifar
 
+# Hyperparams, added defualt values
 batch_size = 16
 lr = 0.1
 gpu = None
+momentum = 0.9
+weight_decay = 1e-4
+start_epoch = 0
+epochs = 100
+
+
+'''
+Load data
+'''
+print("Loading CIFAR...")
+train_loader, test_loader = load_cifar()
 
 
 '''
@@ -14,8 +28,15 @@ Create and train a simsiam model
 '''
 print("Creating SimSiam model...")
 simsiam =  models.simsiam_builder.SimSiam()
-
 # infer learning rate before changing batch size
 init_lr = lr * batch_size / 256
 # define loss function (criterion) and optimizer
 criterion = nn.CosineSimilarity(dim=1).cuda(gpu)
+optim_params = simsiam.parameters()
+optimizer = torch.optim.SGD(optim_params, init_lr,
+                                momentum=momentum,
+                                weight_decay=weight_decay)
+
+for epoch in range(start_epoch, epochs):
+        adjust_learning_rate(optimizer, init_lr, epoch, epochs)
+        train_simsiam(train_loader, simsiam, criterion, optimizer, epoch, gpu)
--- train.py
+++ train.py
@@ -1,5 +1,20 @@
+import math
 
+'''
+From https://github.com/facebookresearch/simsiam/blob/main/main_simsiam.py
+'''
+def adjust_learning_rate(optimizer, init_lr, epoch, epochs):
+    """Decay the learning rate based on schedule"""
+    cur_lr = init_lr * 0.5 * (1. + math.cos(math.pi * epoch / epochs))
+    for param_group in optimizer.param_groups:
+        if 'fix_lr' in param_group and param_group['fix_lr']:
+            param_group['lr'] = init_lr
+        else:
+            param_group['lr'] = cur_lr
 
+'''
+Function to train SimSiam net
+'''
 def train_simsiam(train_loader, model, criterion, optimizer, epoch, gpu):
     for i, (images, _) in enumerate(train_loader):
 
